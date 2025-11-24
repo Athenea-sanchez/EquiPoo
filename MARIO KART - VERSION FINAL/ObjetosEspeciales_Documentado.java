315d16029ac1171eeedd3c81f8ce2467841b3072
Castillo Ramírez Constanza <cons.cr26@gmail.com>
2025-11-23 20:58:27 -0600

    Create ObjetosEspeciales class for special game objects
    This class defines special objects used in the game, including constants for their effects and methods to apply them.

---
+++ MARIO KART - VERSION FINAL/ObjetosEspeciales_Documentado.java
@@ -0,0 +1,44 @@
+/*
+*SUBLCLASE #2 : OBJETOS ESPECIALES
+
+*Clase encargada de almacenar los objetos especiales usados durante el juego.
+*Define los métodos que aplican el castigo de dicho objeto.
+*/
+
+public class ObjetosEspeciales {
+
+    /*
+    *Constante que representa el objeto que reduce el número de casillas por avanzar.
+    */
+    public static final int REDUCE_DADO = 1;
+    /*
+    *Constante que representa el objeto que bloquea a un jugador.
+    */
+    public static final int BLOQUEO = 2;
+    /*
+    *Constante que indica el número de turnos que permanece el efecto del caparazón.
+    */
+    public static final int DURACION_CAPARAZON_TURNOS = 3;
+    /*
+    *Constante que indica el número de turnos que permanece el efecto de la banana.
+    */
+    public static final int DURACION_BANANA_TURNOS = 3;
+
+    /*
+    *Método que regresa el valor de la duración del caparazón.
+    @return : La duración del caparazón.
+    */
+
+    public static int aplicarCaparazon() {
+        return DURACION_CAPARAZON_TURNOS;
+    }
+
+    /*
+    *Método que regresa el valor de la duración de la banana.
+    @return : La duración de la banana.
+    */
+
+    public static int aplicarBanana() {
+        return DURACION_BANANA_TURNOS;
+    }
+}
